328d3a26840ed1fcc51459a27bc5c28b7a413b94
csslick <digitone@naver.com>
2024-05-08 01:54:52 +0900

    48.서체 및 화면 디자인

--- src/lib/components/GameScore.svelte
+++ src/lib/components/GameScore.svelte
@@ -3,6 +3,29 @@
 </script>
 
 <main>
-  <h1>Game Score</h1>
-  <button on:click={() => page="title" }>Back to home</button>
+  <h1 class="itim-regular">Game Score</h1>
+  <div class="score">
+    <p class="hi-score">Hi score: <span>999,999,999</span></p>
+    <p class="your-score">Your score: <span>999,999</span></p>
+  </div>
+  <button 
+    class="btn itim-regular"
+    on:click={() => page="title" }>Back to home</button>
 </main>
+
+<style lang='scss'>
+  main {
+    h1 {
+      font-size: 12vw;
+      color: #ff3e00;
+      -webkit-text-stroke: 10px rgba(255,255,255, 1);
+      paint-order: stroke fill;
+    }
+    .score {
+      font-size: 36px;
+      font-weight: bold;
+      margin: 70px 0;
+      p { margin-bottom: 10px; }
+    }
+  }
+</style>
--- src/lib/components/GameTitle.svelte
+++ src/lib/components/GameTitle.svelte
@@ -7,17 +7,42 @@
 </script>
 
 <main>
-  {#if page === 'title'}
-    <section>
+  {#if page === "title"}
+    <section class="itim-regular">
       <h1>{title}</h1>
       <div class="btn-group">
-        <button on:click={()=> page = "play"}>Start</button>
-        <button on:click={()=> page = "score"}>Score</button>
+        <button class="btn" on:click={() => (page = "play")}>Start</button>
+        <button class="btn" on:click={() => (page = "score")}>Score</button>
       </div>
     </section>
-  {:else if page === 'play'}
+  {:else if page === "play"}
     <GamePlay />
-  {:else if page ==='score'}
-    <GameScore bind:page={page} />
+  {:else if page === "score"}
+    <GameScore bind:page />
   {/if}
 </main>
+
+<style lang="scss">
+  section {
+    h1 {
+      font-size: 16vw;
+      color: #ff3e00;
+      -webkit-text-stroke: 10px rgba(0, 0, 0, 0.5);
+      paint-order: stroke fill;
+    }
+    .btn-group {
+      display: flex;
+      flex-direction: column;
+      margin-top: 40px;
+      button {
+        padding: 10px;
+        font-size: 32px;
+        background: transparent;
+        border: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+</style>
