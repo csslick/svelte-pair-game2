468ffd6bff1e98754d87dddaa10884939cfca618
csslick <digitone@naver.com>
2024-05-21 18:24:56 +0900

    67.게임 점수 기록 저장하고 보여주기

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,6 @@ import { writable } from "svelte/store";
 // store 변수 만들기
 export const score = writable(0);
 export const round = writable(1);
-export const time = writable(20);
+export const time = writable(60);
 export const page = writable('title');
 
